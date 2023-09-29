ef1d634857be8735c1dd5019e5e17b7497728afd
Pedro <pedrogarrido971@gmail.com>
2023-09-29 11:49:06 -0300

    empezados algunos cambios en users. cambiar el insert para que mysql haga el id autoincremental.

--- models/users.js
+++ models/users.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise'
 
 const DEFAULT_CONFIG = {
-  host: 'localhost', 
+  host: 'localhost',
   user: 'root',
   port: 3306,
   password: 'francisco',
-  database: 'tpdsw'  
+  database: 'tpdsw'
 }
 
 const connectionString = process.env.DATABASE_URL ?? DEFAULT_CONFIG
@@ -18,18 +18,18 @@ export class UserModel {
     return users
   }
 
-  static async getUserById({id}) {
+  static async getUserById({ id }) {
     const [users] = await connection.query('select * from usuarios where id_usuario = ?', [id])
     return users[0]
   }
 
-  static async deleteUserById({id}){
+  static async deleteUserById({ id }) {
     const [users] = await connection.query('select * from usuarios where id_usuario = ?', [id])
     await connection.query('delete from usuarios where id_usuario = ?', [id])
     return users[0]
   }
 
-  static async createUser({input}){
+  static async createUser({ input }) {
     const {
       id_usuario,
       nombre_usuario,
@@ -45,4 +45,29 @@ export class UserModel {
     const [newUser] = await connection.query('select * from usuarios where id_usuario = ?', [id_usuario])
     return newUser[0]
   }
+
+  static async modifyUser({ id, input }) {
+    const oldUserContainer = await connection.query('select * from usuarios where id_usuario = ?', [id])
+    if (oldUserContainer.length === 0) {
+      throw new Error("Usuario no encontrado")
+    }
+    const oldUser = oldUserContainer[0][0]
+    const {
+      nombre_usuario = oldUser.nombre_usuario,
+      clave = oldUser.clave,
+      email = oldUser.email,
+      telefono = oldUser.telefono,
+      nombre = oldUser.nombre,
+      apellido = oldUser.apellido,
+      direccion = oldUser.direccion,
+      tipo_usuario = oldUser.tipo_usuario
+    } = input;
+    await connection.query(`update usuarios set nombre_usuario = ?, clave = ?, email = ?, telefono = ?, 
+    nombre = ?, apellido = ?, direccion = ?, tipo_usuario = ? where id_usuario = ?`, [nombre_usuario, clave, email, telefono, nombre, apellido, direccion, tipo_usuario, id])
+    const updatedUser = await connection.query('select * from usuarios where id_usuario = ?', [id])
+    if (!updatedUser) {
+      throw new Error('Error actualizando el usuario')
+    }
+    return updatedUser
   }
+}
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ import { UserController } from "../controllers/users.js";
 export const createUserRouter = ({ userModel }) => {
   const userRouter = Router()
 
-  const userController = new UserController({ userModel})
+  const userController = new UserController({ userModel })
 
   userRouter.get('/', userController.getAllUsers)
   userRouter.get('/:id', userController.getUserById)
   userRouter.delete('/:id', userController.deleteUserById)
-  userRouter.post('/',userController.createUser)
+  userRouter.post('/', userController.createUser)
+  userRouter.patch('/:id', userController.modifyUser)
 
   return userRouter
 }
